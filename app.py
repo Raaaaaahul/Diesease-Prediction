4e7a794d3a5b166ebec98f6456e2b46e332de419
adityarai3 <101617731+adityarai3@users.noreply.github.com>
2023-05-22 12:44:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if (selected == 'Diabetes Prediction'):
     col1, col2, col3 = st.columns(3)
     
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
+        Pregnancies = st.text_input('Number of Pregnancies till date')
         
     with col2:
         Glucose = st.text_input('Glucose Level')
